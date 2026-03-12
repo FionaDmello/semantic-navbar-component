a28f78b9a189c93998a11caf624ce58b735d6249
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-03-12 16:56:12 +0100

    added basic js to make hamburger menu clickable

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+const openMenu = false;
+const button = document.getElementById("menu-button")
+
+button.addEventListener("click", () => {
+  console.log("here", openMenu)
+  openMenu = !openMenu
+}) 
