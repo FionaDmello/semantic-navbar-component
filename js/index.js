da1b9df138e4d8814b4d4dfba8a35f3563189f34
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-03-20 16:15:40 +0100

    this is getting complicated

--- js/index.js
+++ js/index.js
@@ -5,6 +5,19 @@ const closeButton = document.getElementById("close-button")
 const navLinksContainer = document.getElementById("nav-links-container")
 const buttonContainer = document.getElementById("button-container")
 
+if (window.innerWidth >= 1280) {
+  navLinksContainer.classList.remove("hidden")
+  buttonContainer.classList.remove("hidden")
+  closeButton.classList.remove("hidden")
+  menuButton.classList.add("hidden")
+}
+else {
+  navLinksContainer.classList.add("hidden")
+  buttonContainer.classList.add("hidden")
+  closeButton.classList.add("hidden")
+  menuButton.classList.remove("hidden")
+}
+
 menuButton.addEventListener("click", () => {
   navLinksContainer.classList.remove("hidden")
   buttonContainer.classList.remove("hidden")
