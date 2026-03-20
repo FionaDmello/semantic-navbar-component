f6bf2c27bddb0eff1312cc0b46ad88c76868f800
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-03-20 15:43:45 +0100

    basic js functions written for functionality

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
-const openMenu = false;
-const button = document.getElementById("menu-button")
+const menuButton = document.getElementById("menu-button")
+const closeButton = document.getElementById("close-button")
+const navLinksContainer = document.getElementById("nav-links-container")
+const buttonContainer = document.getElementById("button-container")
 
-button.addEventListener("click", () => {
-  console.log("here", openMenu)
-  openMenu = !openMenu
+menuButton.addEventListener("click", () => {
+  console.log("what happened?")
+  navLinksContainer.classList.remove("hidden")
+  navLinksContainer.classList.remove("hidden")
+  closeButton.classList.remove("hidden")
+  menuButton.classList.add("hidden")
 }) 
+
+closeButton.addEventListener("click", () => {
+  navLinksContainer.classList.add("hidden")
+  navLinksContainer.classList.add("hidden")
+  closeButton.classList.add("hidden")
+  menuButton.classList.remove("hidden")
+})
